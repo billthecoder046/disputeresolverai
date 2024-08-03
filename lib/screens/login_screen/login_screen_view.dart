6ae0217a83b88f5e440ae5572c6afe4efb72dc58
BilalSaeed <bilal.saeed@catalyic.com>
2024-08-03 19:34:46 +0500

    Modularized Widgets added

--- lib/screens/login_screen/login_screen_view.dart
+++ lib/screens/login_screen/login_screen_view.dart
@@ -1,4 +1,7 @@
+import 'package:disputeresolverai/screens/commonWidgets/buttons.dart';
+import 'package:disputeresolverai/screens/commonWidgets/fieldWidgets.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
@@ -10,27 +13,64 @@ class Login_screenPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenType = ResponsiveBreakpoints.of(context);
-    return Container(
-      color: Colors.yellow,
-      child: Center(
-        child: screenType.largerThan(TABLET)
-            ? showWebLoginScreen()
-            : screenType.largerThan(MOBILE)
-            ? showTabLoginScreen()
-            : showMobileLoginScreen(),
+    return Scaffold(
+      body: Container(
+        color: Colors.yellow,
+        child: Center(
+          child: screenType.largerThan(TABLET)
+              ? showWebLoginScreen(context)
+              : screenType.largerThan(MOBILE)
+              ? showTabLoginScreen(context)
+              : showMobileLoginScreen(context),
+        ),
       ),
     );
   }
 
-  showMobileLoginScreen() {
-    return const Text("My Mobile Screen");
+  showMobileLoginScreen(BuildContext context) {
+    return myForm(context);
   }
 
-  showWebLoginScreen() {
-    return const Text("My Web Login Screen");
+  showWebLoginScreen(context) {
+    return myForm(context);
   }
 
-  showTabLoginScreen() {
-    return const Text("My Tab Login Screen");
+  showTabLoginScreen(context) {
+    return myForm(context);
   }
+
+  myForm(context){
+    return   Column(
+      children: [
+        Gap(16),
+        MyTextField(
+            myController: logic.emailC,
+            hintText: "Enter Email"
+        ),
+        Gap(16),
+        MyTextField(
+          myController: logic.passC,
+          hintText: "Enter Password"
+        ),
+        Gap(16),
+        myFirstButton(
+          myFunction: () {
+            print("My Email Data: ${logic.emailC.text}");
+            print("My Password Data: ${logic.passC.text}");
+          },
+          myButtonWidget: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.ads_click),
+              Gap(6),
+              Text("Signin")
+            ],
+          ),
+
+        )
+
+      ],
+    );
+  }
+
 }
