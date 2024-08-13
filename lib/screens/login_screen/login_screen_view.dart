9beacc343262bad8b539c08f6aace3db8b37a13f
MianKashanJh045 <miankashanjh0047@gmail.com>
2024-08-13 01:56:02 -0700

    changings

--- lib/screens/login_screen/login_screen_view.dart
+++ lib/screens/login_screen/login_screen_view.dart
@@ -49,6 +49,13 @@ class Login_screenPage extends StatelessWidget {
   mySignInForm(context) {
     return Column(
       children: [
+        Container(
+          height: 200,
+          width: 200,
+          child: Image.asset('assets/images/pngegg (1).png'),
+        ),
+        //ap name
+        Text('º•DeisputeResolverAI•º',style: TextStyle(fontSize: 20,fontWeight:FontWeight.bold),),
         Gap(16),
         Text("Sign In Form",style: MyTextStyles.myTextStyleBlueLarge,),
         Gap(16),
@@ -83,6 +90,13 @@ class Login_screenPage extends StatelessWidget {
   mySignUpForm(context) {
     return Column(
       children: [
+        Container(
+          height: 200,
+          width: 200,
+          child: Image.asset('assets/images/pngegg (1).png'),
+        ),
+        //ap name
+        Text('º•DeisputeResolverAI•º',style: TextStyle(fontSize: 20,fontWeight:FontWeight.bold),),
         Gap(16),
         Text("Sign Up Form",style: MyTextStyles.myTextStyleBlueLarge,),
         Gap(16),
