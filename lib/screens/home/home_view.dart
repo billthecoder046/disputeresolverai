43f54a9289e0a758d167ffee4c549725c68b12bf
Malik_Sarkar <adreesking975@gmail.com>
2025-01-07 18:06:17 +0500

    muje nahi pata kihoya code me

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -1,13 +1,10 @@
 import 'dart:js_interop';
-import 'dart:math';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
 import '../../model/users.dart';
-import 'chat_Page/chat_screen.dart';
 import 'home_logic.dart';
 
 class Home_screenPage extends StatelessWidget {
@@ -31,83 +28,72 @@ class Home_screenPage extends StatelessWidget {
               itemBuilder: (context, i) {
                 DateTime dateTime;
                 if (logic.myAllStudets[i].createdAt.runtimeType == int) {
-                  dateTime = DateTime.fromMicrosecondsSinceEpoch(
-                      logic.myAllStudets[i].createdAt);
+                  dateTime = DateTime.fromMicrosecondsSinceEpoch(logic.myAllStudets[i].createdAt);
                 } else {
-                  dateTime = DateTime.parse(
-                      logic.myAllStudets[i].createdAt.toString());
+                  dateTime = DateTime.parse(logic.myAllStudets[i].createdAt.toString());
                 }
-                DateTime adjustedDateTime =
-                    dateTime.subtract(Duration(days: 20));
+                DateTime adjustedDateTime = dateTime.subtract(Duration(days: 20));
                 String papuDateAndTime =
-                    DateFormat.yMd().add_jm().format(adjustedDateTime);
-                return FirebaseAuth.instance.currentUser!.uid ==
-                        logic.myAllStudets[i].id
-                    ? Container()
-                    : Column(
-                        children: [
-                          Card(
-                            margin: EdgeInsets.symmetric(vertical: 10),
-                            elevation: 20,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15),
+                DateFormat.yMd().add_jm().format(adjustedDateTime);
+                return Column(
+                  children: [
+                    Card(
+                      margin: EdgeInsets.symmetric(vertical: 10),
+                      elevation: 20,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.shade300,
+                              blurRadius: 10,
+                              offset: Offset(0, 5),
                             ),
-                            child: Container(
-                              padding: EdgeInsets.all(15),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(15),
-                                color: Colors.white,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey.shade300,
-                                    blurRadius: 10,
-                                    offset: Offset(0, 5),
-                                  ),
-                                ],
-                              ),
-                              child: InkWell(
-                                onTap: (){
-                                  Get.to(()=>ChatScreen());
-                                },
-                                child: ListTile(
-                                  title: Text(
-                                    logic.myAllStudets[i].name,
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.teal.shade900,
-                                    ),
-                                  ),
-                                  leading: GestureDetector(
-                                    onTap: () {
-                                      showDialog(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return Dialog(
-                                            child: Container(
-                                              padding: EdgeInsets.all(10),
-                                              child: Image.network(
-                                                logic.myAllStudets[i].imageUrl,
-                                                fit: BoxFit.contain,
-                                              ),
-                                            ),
-                                          );
-                                        },
-                                      );
-                                    },
-                                    child: Image.network(
-                                      logic.myAllStudets[i].imageUrl,
-                                      height: 120,
-                                      width: 120,
-                                    ),
-                                  ),
-                                  subtitle: Text('$papuDateAndTime'),
-                                ),
-                              ),
+                          ],
+                        ),
+                        child: ListTile(
+                          title: Text(
+                            logic.myAllStudets[i].name,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.teal.shade900,
                             ),
                           ),
-                        ],
-                      );
+                          leading: GestureDetector(
+                            onTap: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return Dialog(
+                                    child: Container(
+                                      padding: EdgeInsets.all(10),
+                                      child: Image.network(
+                                        logic.myAllStudets[i].imageUrl,
+                                        fit: BoxFit.contain,
+                                      ),
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                            child: Image.network(
+                              logic.myAllStudets[i].imageUrl,
+                              height: 120,
+                              width: 120,
+                            ),
+                          ),
+                          subtitle: Text('$papuDateAndTime'),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
               },
             ),
           );
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,8 +1,8 @@
-import 'package:disputeresolverai/screens/login_screen/sign_up_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../screens/home/home_view.dart';
+import '../screens/signup_screen/sign_up_screen.dart';
 
 class AuthWrapper extends StatelessWidget {
   const AuthWrapper({super.key});
